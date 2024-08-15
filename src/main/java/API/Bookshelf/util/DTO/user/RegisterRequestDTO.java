9e277964b8f770cdf59445f3740ce9a7b55b02fa
Axel Barlian <axelbarlian16@gmail.com>
2024-08-15 21:39:47 +0700

    [Updated] using regex pattern

--- src/main/java/API/Bookshelf/util/DTO/user/RegisterRequestDTO.java
+++ src/main/java/API/Bookshelf/util/DTO/user/RegisterRequestDTO.java
@@ -2,6 +2,7 @@ package API.Bookshelf.util.DTO.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +17,7 @@ public class RegisterRequestDTO {
     private String name;
     @NotBlank(message = "Email can't be blank")
     @Email(message = "Invalid email format")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email format")
     private String email;
     @NotBlank(message = "Password can't be blank")
     private String password;
